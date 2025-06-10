98e1042684f3ad0a4908c52c4af2844c3c43e3a4
datagucc <a.nollevaux.data@gmail.com>
2025-06-10 15:33:18 -0300

    commiting update ETL

--- dags/ETL_dag.py
+++ dags/ETL_dag.py
@@ -1,4 +1,4 @@
-# 1) IMPORTATION DES MODULES
+# Import necessary libraries and modules
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
@@ -9,41 +9,52 @@ from airflow.utils.dates import days_ago
 from datetime import datetime
 from datetime import timedelta
 
+# 0) On doit commencer par expliquer le docker-compose, l'utilisation des volumes, etc.. (pour ensuite réexpliquer ici qu'on 
+# reprend les volumes qui ont été montés dans le docker-compose)
 
-#on définit nos arguments par défaut.
+# A) We define the default arguments for the DAG.
 default_args = {
-    'owner': 'Augustin'
-    ,'start_date': datetime(2025, 1, 1)  # The date when the DAG starts
-    ,'retries':2
-    ,'retry_delay': timedelta(minutes=3)
+    'owner': 'Augustin'                  # owner of the DAG : the person who created it
+    ,'start_date': datetime(2025, 1, 1)  # define when the DAG should start running
+    ,'email_on_failure': False            # to send an email on failure : we do not use it here
+    ,'email_on_success': False            # to send an email on success : we do not use it here
+    ,'email_on_retry': False              # to send an email on retry : we do not use it here
+    ,'retries':2                         # number of retries in case of failure 
+    ,'retry_delay': timedelta(minutes=3)  # delay between retries 
 }
 
-# Define the DAG
+# B) We define the DAG itself.
 ETL_dag = DAG(
-    dag_id = 'ETL_analytics_sport_shop',             # DAG ID
-    default_args=default_args,     # Default arguments for the DAG
-    description='Analytics ETL',
-    tags=['Data Engineering courses',"Advanced"],
-    catchup= False,              # Do not backfill past runs when DAG is created
-    schedule='0 0 * * *'  # Schedule interval (run at midnight every day)
+    dag_id = 'ETL_analytics_sport_shop',                               # DAG ID
+    default_args=default_args,                                         # Default arguments for the DAG (defined above)
+    description='ETL process for the analytics of the sport shop',     # Description of the DAG
+    tags=['ETL','PostgreSQL','Analytics','Docker','Flatfile','JSON'],  # Tags for the DAG
+    catchup= False,                                                    # Do not backfill past runs when DAG is created 
+                                                                       # Backfill means that Airflow will try to run the DAG for all past dates
+    schedule='0 0 * * *'                                               # Schedule interval (run at midnight every day)
+                                                                        # https://crontab.guru/ : very useful website to understand cron expressions
 )
 
 
 
+# C) We define the tasks in the DAG.
 
-
-# The start point of the DAG, represented by a DummyOperator
-# Cet opérateur Dummy permet de centraliser le point de départ.
+# 1) The start point of the DAG, represented by a DummyOperator
+# expliquer à quoi il sert
 start = DummyOperator(
     task_id='start',
     dag=ETL_dag
 )
 
-# The first task, extract and load data from JSON to Python, represented by a BashOperator
-# A) On extrait les données de json. On le fait à partir de l'opérateur bash.
-# Plutot que de lancer un code python, je vais plutot créer un code python à l'exterieur de ce DAG
-# et ensuite mon opérateur bash va aller chercher ce script python et l'exécuter.
-#on considère cette tâche comme critique donc on ajout un execution timeout:
+
+# A good practice of Airflow is to separate the tasks into different scripts, and use a BashOperator to call these scripts.
+# Indeed, it is easier to maintain and debug the code this way. 
+# In our case, we will divide our ETL process into several scripts, each one responsible for a specific task:
+# 1) Extract and transform the JSON file into a big table in PostgreSQL
+# 2) Verify if the big table exists in PostgreSQL (this is just a check task) --> à supprimer?
+# 3) From the big table, we will create several tables in PostgreSQL : store_sales, daily_sales and payment_method. 
+# We will have a separate script for each table. Each script will retrieve the data from the big table in postgreSQL,
+# transform it and load it into the corresponding table in PostgreSQL.
 extract_and_transform_JSON = BashOperator(
     task_id='extract_transform_big_table'
     ,bash_command='python /opt/airflow/scripts/extract_transform_big_table.py'
@@ -77,6 +88,8 @@ transform_payment_method = BashOperator(
     dag=ETL_dag
 )
 
+# 3) Once the tables are created, we will do some analysis and measurements on the data.
+    # We will store these analysis into views in postgreSQL, to be used in further analysis or in reporting tools like PowerBI.
 analysis_daily_sales = BashOperator(
     task_id='analysis_daily_sales',
     bash_command='python /opt/airflow/scripts/analysis_daily_sales.py',
@@ -95,13 +108,15 @@ analysis_payment_method = BashOperator(
     dag=ETL_dag
 )
 
-# The end point of the DAG, represented by a DummyOperator
-# On crée une dernière tache qui nous permet de centraliser le stop. C'est pour centraliser le point d'arret.
+# 4) The end point of the DAG, represented by a DummyOperator
+# expliquer à quoi il sert 
 stop = DummyOperator(
     task_id='stop',
     dag=ETL_dag
 )
 
+
+# D) We define the dependencies between the tasks in the DAG.
 start >> extract_and_transform_JSON
 
 extract_and_transform_JSON >> transform_store_sales
